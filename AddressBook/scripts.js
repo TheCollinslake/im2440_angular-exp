ccfa6bed66420fdb46637c97aab26088a8135214
TheCollinslake <lakefeb2345@gmail.com>
2017-03-16 19:41:06 -0500

    changed the controller

--- AddressBook/scripts.js
+++ AddressBook/scripts.js
@@ -1,35 +1,145 @@
-function PeopleController($scope){
+/*function PeopleController($scope) {
 
     $scope.people=[
+
         {
-            fname: "Caleb",
-            lname: "Collins",
-            phone: "6418161234",
-            city: "Greene",
-            state: "Iowa",
-            email: "lakefeb2345@gmail.com",
+            fname: "Calvin",
+            lname: "Bast",
+            phone: "3452345678",
+            city: "New York",
+            state: "New York",
+            email: "calvin@gmail.com",
             gender: "M",
         },
         {
             fname: "Travis",
             lname: "Roberts",
-            phone: "1234567890",
-            city: "Minniapoles",
-            state: "Minnasota",
+            phone: "1236548769",
+            city: "Chicago",
+            state: "Illinois",
             email: "travis@gmail.com",
             gender: "M",
+        },
+        {
+            fname: "Meg",
+            lname: "Gautier",
+            phone: "1236548769",
+            city: "Chicago",
+            state: "Illinois",
+            email: "travis@gmail.com",
+            gender: "F",
+        },
+        {
+            fname: "Caleb",
+            lname: "Collins",
+            phone: "4567853432",
+            city: "Los Angeles",
+            state: "Cali",
+            email: "caleb@gmail.com",
+            gender: "M",
+        },
+        {
+            fname: "Kevin",
+            lname: "Firmin",
+            phone: "9025673152",
+            city: "Las Vegas",
+            state: "Nevada",
+            email: "kevin@gmail.com",
+            gender: "M",
         }
+
     ];
 
+
     $scope.Save = function() {
-        $scope.people.push({
-            fname: $scope.newPerson.fname,
-            lname: $scope.newPerson.lname,
-            phone: $scope.newPerson.phone,
-            city: $scope.newPerson.city,
-            state: $scope.newPerson.state,
-            email: $scope.newPerson.email,
-            gender: $scope.newPerson.gender,
-        });
+        $scope.people.push(
+            {
+                fname: $scope.newPerson.fname,
+                lname: $scope.newPerson.lname,
+
+                phone: $scope.newPerson.phone,
+
+                city: $scope.newPerson.city,
+                state: $scope.newPerson.state,
+
+                email: $scope.newPerson.email,
+
+                gender: $scope.newPerson.gender,
+            }
+        );
     }
+
 }
+*/
+angular.module("addbook",[])
+        .controller('PeopleController',['$scope', function($scope) {
+            $scope.people=[
+
+                {
+                    fname: "Calvin",
+                    lname: "Bast",
+                    phone: "3452345678",
+                    city: "New York",
+                    state: "New York",
+                    email: "calvin@gmail.com",
+                    gender: "M",
+                },
+                {
+                    fname: "Travis",
+                    lname: "Roberts",
+                    phone: "1236548769",
+                    city: "Chicago",
+                    state: "Illinois",
+                    email: "travis@gmail.com",
+                    gender: "M",
+                },
+                {
+                    fname: "Meg",
+                    lname: "Gautier",
+                    phone: "1236548769",
+                    city: "Chicago",
+                    state: "Illinois",
+                    email: "travis@gmail.com",
+                    gender: "F",
+                },
+                {
+                    fname: "Caleb",
+                    lname: "Collins",
+                    phone: "4567853432",
+                    city: "Los Angeles",
+                    state: "Cali",
+                    email: "caleb@gmail.com",
+                    gender: "M",
+                },
+                {
+                    fname: "Kevin",
+                    lname: "Firmin",
+                    phone: "9025673152",
+                    city: "Las Vegas",
+                    state: "Nevada",
+                    email: "kevin@gmail.com",
+                    gender: "M",
+                }
+
+            ];
+
+
+            $scope.Save = function() {
+                $scope.people.push(
+                    {
+                        fname: $scope.newPerson.fname,
+                        lname: $scope.newPerson.lname,
+
+                        phone: $scope.newPerson.phone,
+
+                        city: $scope.newPerson.city,
+                        state: $scope.newPerson.state,
+
+                        email: $scope.newPerson.email,
+
+                        gender: $scope.newPerson.gender,
+                    }
+                );
+            }
+
+        }]);
